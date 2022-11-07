00565e65e06cfdca33df22c0838b1efb979135d4
satibekov <satibekov52@gmail.com>
2022-11-07 10:43:17 +0600

    yguhijo

---
+++ main.js
@@ -0,0 +1,13 @@
+const TodoListTasks = [];
+
+var todoListAddBtn = document.getElementById('todo-list-task-add-btn')
+
+todoListAddBtn.addEventListener('click', AddTodo)
+
+function AddTodo() {
+    var todoListTaskText = document.getElementById
+    ('todo-list-task-text').value;
+    console.log(todoListTaskText)
+
+
+}
